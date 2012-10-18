28823c025a951b85927d201fa90011bfbb87ae64
dhacker29 <davidhackerdvm@gmail.com>
2012-10-18 18:51:09 -0500

    Allow CNA build

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,3 +2,6 @@ PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_edison.mk
 ifeq ($(TARGET_PRODUCT),aokp_edison)
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/aokp.mk
 endif
+ifeq ($(TARGET_PRODUCT),cna_edison)
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/cna.mk
+endif
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -22,4 +22,5 @@
 
 add_lunch_combo aokp_edison-userdebug
 add_lunch_combo cm_edison-userdebug
+add_lunch_combo cna_edison-userdebug
 add_lunch_combo full_edison-userdebug
