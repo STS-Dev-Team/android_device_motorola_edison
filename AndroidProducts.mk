6d8d20b10042785502a7a9082e1209dd2cc0d204
Hashcode <hashcode0f@gmail.com>
2013-06-26 10:51:20 -0700

    add copyright notices
    Change-Id: Iff7345a01e59a38dbdfe3ffe26ded811792cdf0f

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,15 @@
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_edison.mk
-ifeq ($(TARGET_PRODUCT),aokp_edison)
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/aokp.mk
-endif
-ifeq ($(TARGET_PRODUCT),cna_edison)
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/cna.mk
-endif
-ifeq ($(TARGET_PRODUCT),killrom_edison)
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/killrom.mk
-endif
