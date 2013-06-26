bf1735eef5a9cda082bb2e636608e55a3b912d63
Hashcode <hashcode0f@gmail.com>
2013-06-26 10:50:15 -0700

    remove vendorsetup lines for extra ROMs
    Change-Id: Icb7ede3958a3d3de258c94f7fd533c151a53b1d0

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2008 The Android Open Source Project
+# Copyright (C) 2013 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,8 +20,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo aokp_edison-userdebug
 add_lunch_combo cm_edison-userdebug
-add_lunch_combo cna_edison-userdebug
-add_lunch_combo full_edison-userdebug
-add_lunch_combo killrom_edison-userdebug
